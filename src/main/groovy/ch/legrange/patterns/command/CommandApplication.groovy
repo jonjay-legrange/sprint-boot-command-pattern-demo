c9fb2afd938e1b99d62acec15d17cb099c5eef0c
Jon Jay Le Grange <jonjay.legrange@blv.admin.ch>
2022-06-12 13:51:46 +0200

    Setup Spring Boot

---
+++ src/main/groovy/ch/legrange/patterns/command/CommandApplication.groovy
@@ -0,0 +1,13 @@
+package ch.legrange.patterns.command
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CommandApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CommandApplication, args)
+	}
+
+}
---
+++ src/test/groovy/ch/legrange/patterns/command/CommandApplicationTests.groovy
@@ -0,0 +1,13 @@
+package ch.legrange.patterns.command
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
